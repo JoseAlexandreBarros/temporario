79364d191ff9754504c93c340f61a87899ecb67a
JoseAlexandreBarros <josealexandrebarros1990@gmail.com>
2023-08-08 13:45:20 -0300

    temp

--- autenticacao/settings.py
+++ autenticacao/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-(ofzci62rq&5rms1-&e9!8(w(oc+(e1c5!a5!!=h5b!r23*z_6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.vercel.app']
 
